2c07a03e721b349e97c6b37c070f421c2421fb44
Liam Shaw <liam.philip.shaw@gmail.com>
2023-10-20 11:03:34 +0100

    k=4 and k=6

--- scripts/submit-rmes-jobs.sh
+++ scripts/submit-rmes-jobs.sh
@@ -3,5 +3,6 @@
 for f in *fa;
 do
 	name=$(echo $f | cut -d '.' -f 1)
-	sbatch -p short run-rmes.sh $f "$name".csv 6
+	sbatch -p short run-rmes.sh $f "$name"_k4.csv 4
+	sbatch -p short run-rmes.sh $f "$name"_k6.csv 6
 done 

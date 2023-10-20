8e3f7053214a20690e1c7ac80c8d02b915752fe4
Liam Shaw <liam.philip.shaw@gmail.com>
2023-10-20 11:46:11 +0100

    plot for top 50 palidnrome density

--- scripts/make_plot_top50.R
+++ scripts/make_plot_top50.R
@@ -4,8 +4,15 @@ library(ggbeeswarm)
 
 ptu_info = read.csv('../data/top50ptus_info.csv', header=T, stringsAsFactors = F, row.names = 1)
 top50_PTUs = read.csv('../top50-ptus-with-core.txt', header=F, stringsAsFactors = F)$V1
-palindrome_density_df = data.frame(kmer=NA, density=NA, component=NA, PTU=NA)
 
+gc_df = read.csv('../output/PTU_core_accessory_GC.csv', header=T, stringsAsFactors = F)
+
+gc_df_diff = data.frame(gc_df %>% group_by(PTU) %>%
+                          summarise(diff=GC[component=="core"]-GC[component=="accessory"]))
+rownames(gc_df_diff) = gc_df_diff$PTU
+ptu_info$GC.diff = gc_df_diff[rownames(ptu_info),"diff"]
+
+palindrome_density_df = data.frame(kmer=NA, density=NA, component=NA, PTU=NA)
 
 k = 6
 file_suffix = paste0('k', k, '_palindrome_densities.txt')
@@ -37,32 +44,40 @@ palindrome_density_diff$host.range.numeric = as.numeric(palindrome_density_diff$
 palindrome_density_diff$size = ptu_info[as.character(palindrome_density_diff$PTU), "median.length"]
 
 
-ggplot(palindrome_density_diff, aes(host.range, diff))+
-  geom_boxplot(outlier.shape = NA)+
-  ggbeeswarm::geom_quasirandom()+
+p.all = ggplot(palindrome_density_diff, aes(host.range, diff, colour=size>5000))+
+  geom_boxplot(outlier.shape = NA, aes(group=host.range), colour="black")+
+  ggbeeswarm::geom_quasirandom(size=3)+
   theme_bw()+
   xlab("PTU host range")+
   ylab("palindrome density difference (core-accessory)")+
-  theme(axis.text=element_text(size=12, colour="black"), axis.title=element_text(size=18))+
-  ggtitle("All plasmids")
-ggplot(palindrome_density_diff[which(palindrome_density_diff$size>5000),], aes(host.range, diff))+
-  geom_boxplot(outlier.shape = NA)+
-  ggbeeswarm::geom_quasirandom()+
-  theme_bw()+
-  xlab("PTU host range")+
-  ylab("palindrome density difference (core-accessory)")+
-  theme(axis.text=element_text(size=12, colour="black"), axis.title=element_text(size=18))+
-  ggtitle("Median size > 5kb")
-ggplot(palindrome_density_diff[which(palindrome_density_diff$size>20000),], aes(host.range, diff))+
-  geom_boxplot(outlier.shape = NA)+
-  ggbeeswarm::geom_quasirandom()+
-  theme_bw()+
-  xlab("PTU host range")+
-  ylab("palindrome density difference (core-accessory)")+
-  theme(axis.text=element_text(size=12, colour="black"), axis.title=element_text(size=18))+
-  ggtitle("Median size > 20kb")
+  theme(axis.text=element_text(size=8, colour="black"), axis.title=element_text(size=12))+
+  ggtitle("All plasmids")+
+  scale_color_manual(values=c("red", "black"))
+ggsave(p.all, file='../output/palindrome-density-top50-plot.pdf')
 
-summary(lm(diff ~ host.range.numeric:log10(size), data=palindrome_density_diff))
+p.5kb = ggplot(palindrome_density_diff[which(palindrome_density_diff$size>5000),], aes(host.range.numeric, diff))+
+  geom_jitter(size=3, width = 0.05, height=0)+
+  theme_bw()+
+  xlab("PTU host range")+
+  stat_smooth(method="lm")+
+  ylab("palindrome density difference (core-accessory)")+
+  theme(axis.text=element_text(size=8, colour="black"), axis.title=element_text(size=12))+
+  ggtitle("Plasmids>5kb")+
+  guides(color=FALSE)
 
-ggplot(palindrome_density_overall[which(palindrome_density_overall$PTU=="PTU-C"),], aes(component, density))+
-  geom_bar(stat="identity")
+# Is it explained by GC content? apparently not
+palindrome_density_diff$GC.diff = ptu_info[palindrome_density_diff$PTU, "GC.diff"]
+p.GC = ggplot(palindrome_density_diff, aes(GC.diff, diff))+
+  geom_point()+
+  theme_bw()+
+  theme(panel.grid = element_blank())+
+  stat_smooth(method="lm")+
+  theme(axis.text=element_text(size=8, colour="black"), axis.title=element_text(size=12))+
+  xlab("GC difference (core - accessory)")+
+  ylab("Palindrome density difference (core - accessory)")+
+  ggtitle("No strong GC-associated signal")
+
+p.combined = cowplot::plot_grid(p.all, p.5kb,p.GC, nrow=1,  rel_widths = c(1.2, 1, 1))
+ggsave(p.combined, file='../output/palindrome-density-top50-plot.pdf', width=12, height=6)
+
+
